775cd8f5dcdb92145388d1abf4d6f1c1183917f1
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2022-08-19 04:53:05 +0700

    fix: mengubah tampilan masuk ke chakra ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-import "picnic";
+import { ChakraProvider } from "@chakra-ui/react";
 
 import App from "./App";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -3,7 +3,19 @@ import { Link } from "react-router-dom";
 
 import type { PortInfo } from "@/types/global.d";
 
-import styles from "styles/pages/Home.module.css";
+import {
+  Flex,
+  Stack,
+  Box,
+  Heading,
+  Text,
+  Button,
+  List,
+  ListItem,
+  ListIcon,
+} from "@chakra-ui/react";
+import { IoRocketOutline } from "react-icons/io5";
+import { MdUsb, MdArrowRightAlt } from "react-icons/md";
 
 export const Home = () => {
   const [arduinos, setArduinos] = useState<null | PortInfo[]>(null);
@@ -21,40 +33,73 @@ export const Home = () => {
   }, []);
 
   return (
-    <div className={styles.container}>
-      <div>
-        <article className={`card ${styles.cardStyle}`}>
-          <footer>
-            <h1>List Arduino</h1>
-            {!arduinos && <p>Sedang mengambil data...</p>}
-            {arduinos && arduinos.length < 1 && (
-              <>
-                <p>Tidak ditemukan adanya arduino penerima telemetri</p>
-                <button
-                  onClick={() => {
-                    setArduinos(null);
-                    window.telemetryAPI.sendListArduinoReciever();
-                  }}
-                >
-                  Ambil data ulang
-                </button>
-              </>
-            )}
-            {arduinos && arduinos.length > 0 && (
-              <ul>
-                {arduinos.map((arduino) => (
-                  <li key={arduino.path}>
-                    {/* eslint-disable-next-line @typescript-eslint/no-non-null-assertion */}
-                    <Link to={`viewer/${encodeURIComponent(arduino.path!)}`}>
+    <Flex align="center" justify="center" width="100vw" height="100vh">
+      <Stack spacing={10}>
+        <Heading
+          as="h2"
+          size="3xl"
+          style={{ display: "inline-flex", justifyContent: "space-around" }}
+        >
+          Roket Hiber <IoRocketOutline />
+        </Heading>
+        <Box
+          p={5}
+          minH="300px"
+          minW="488px"
+          borderWidth="1px"
+          borderRadius="lg"
+          overflow="hidden"
+        >
+          <Stack spacing={5} height="100%">
+            <Heading as="h3" size="lg">
+              List Arduino
+            </Heading>
+            <Box>
+              {!arduinos && <Text fontSize="lg">Sedang mengambil data...</Text>}
+              {arduinos && arduinos.length < 1 && (
+                <>
+                  <Text fontSize="lg">
+                    Tidak ditemukan adanya arduino penerima telemetri
+                  </Text>
+                  <Button
+                    colorScheme="blue"
+                    marginTop="2"
+                    onClick={() => {
+                      setArduinos(null);
+                      window.telemetryAPI.sendListArduinoReciever();
+                    }}
+                  >
+                    List Ulang
+                  </Button>
+                </>
+              )}
+              {arduinos && arduinos.length > 0 && (
+                <List spacing={4}>
+                  {arduinos.map((arduino) => (
+                    <ListItem fontSize="lg" key={arduino.path}>
+                      <ListIcon as={MdUsb} color="green.500" />
                       {arduino.path}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            )}
-          </footer>
-        </article>
-      </div>
-    </div>
+
+                      <Button
+                        colorScheme="blue"
+                        marginLeft="179px"
+                        rightIcon={
+                          <MdArrowRightAlt style={{ fontSize: "2.05em" }} />
+                        }
+                        as={Link}
+                        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                        to={`viewer/${encodeURIComponent(arduino.path!)}`}
+                      >
+                        Pantau
+                      </Button>
+                    </ListItem>
+                  ))}
+                </List>
+              )}
+            </Box>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
   );
 };
