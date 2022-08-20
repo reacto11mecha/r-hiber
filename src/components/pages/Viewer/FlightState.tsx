e7dfa5dd6a5d14b912d36714283b3e7f2d39a7da
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2022-08-20 11:37:55 +0700

    fix(types): mengubah props align menjadi style textAlign

--- src/components/pages/Viewer/FlightState.tsx
+++ src/components/pages/Viewer/FlightState.tsx
@@ -3,7 +3,7 @@ import { Box, VStack, Heading, Text } from "@chakra-ui/react";
 export const FlightState = () => (
   <Box h="33%" w="100%" backgroundColor="tomato" p={5}>
     <VStack h="100%" alignItems="center" justifyContent="center">
-      <Heading as="h3" size="lg" align="center">
+      <Heading as="h3" size="lg" style={{ textAlign: "center" }}>
         Flight State
       </Heading>
 
